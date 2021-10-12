5c7d314476fd69f11a533691b96a7963b05913fe
Johanns04 <92264422+Johanns04@users.noreply.github.com>
2021-10-11 21:10:07 -0400

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
+var bGround = require('fcc-express-bground');
+require('dotenv').config();
 
+console.log("Hello world");
 
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
+app.get("/json", (req, res) =>{
+  console.log(process.env.MESSAGE_STYLE, "<= MESSAGE STYLE");
+  var response = {"message":"hello json"};
+  
+  if(process.env.MESSAGE_STYLE === "uppercase"){
+    response.message = response.message.toUpperCase();
+  }
+  
+  res.json(response);
+  
+});
 
 
 
